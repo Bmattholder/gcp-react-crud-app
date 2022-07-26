a8c456170d291ec462940e745e23f1ddd322c751
Matthew Holder <60249613+Bmattholder@users.noreply.github.com>
2022-07-25 22:05:52 -0500

    Update form (#2)
    * adds state for other fields in db
    
    * mostly there
    
    * Create and Read

--- server/index.js
+++ server/index.js
@@ -1,23 +1,22 @@
 // Import express
-const express = require('express'); 
+const express = require('express');
 
 // for parsing the body of the request
-const bodyParser = require('body-parser'); 
+const bodyParser = require('body-parser');
 
 /* Cross-Origin Resource Sharing - middleware for Express. 
 Allows you to share data with a different origin. */
-const cors = require('cors'); 
+const cors = require('cors');
 
 // create our app
-const app = express(); 
+const app = express();
 // import the mysql module
-const mysql = require('mysql'); 
+const mysql = require('mysql');
 
 // use the cors middleware
-app.use(cors()); 
+app.use(cors());
 // use the body parser middleware
-app.use(express.json()); 
-
+app.use(express.json());
 
 const db = mysql.createConnection({
   host: 'localhost',
@@ -27,13 +26,12 @@ const db = mysql.createConnection({
 });
 
 // for parsing application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true })); 
-
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Routes
 
 // get all customers
-app.get('/api/get', (req, res) => { 
+app.get('/api/get', (req, res) => {
   const sqlSelect = 'SELECT * FROM client_data';
   db.query(sqlSelect, (err, result) => {
     res.send(result);
@@ -41,41 +39,89 @@ app.get('/api/get', (req, res) => {
 });
 
 // Post to the db.  req.body is the data sent in the request
-app.post('/api/insert', (req, res) => { 
+app.post('/api/insert', (req, res) => {
   const fullname = req.body.fullname;
   const amount = req.body.amount;
+  const id = req.body.ID;
+  const streetAddress = req.body.street_address;
+  const city = req.body.city;
+  const state = req.body.state;
+  const zip = req.body.zip;
+  const email = req.body.email;
+  const paid = req.body.paid;
+  const dueDate = req.body.due_date;
+  const pastDue = req.body.past_due;
+  const sqlInsert = `INSERT INTO client_data (fullname, amount, id, street_address, city, state, zip, email, paid, due_date, past_due) 
+    VALUES ('${fullname}', '${amount}', '${id}', '${streetAddress}', '${city}', '${state}', '${zip}', '${email}', '${paid}', '${dueDate}', '${pastDue}')`;
 
-  const sqlInsert =
-    'INSERT INTO client_data (fullname, amount) VALUES (?, ?)';
-  db.query(sqlInsert, [fullname, amount], (err, result) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(result);
+  db.query(
+    sqlInsert,
+    [
+      fullname,
+      amount,
+      id,
+      streetAddress,
+      city,
+      state,
+      zip,
+      email,
+      paid,
+      dueDate,
+      pastDue,
+    ],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(result);
+      }
     }
-  });
-});
- 
-// :fullname is a placeholder for the id
-app.delete('/api/delete/:fullname', (req, res) => { 
-  const fullname = req.params.fullname;
-  const sqlDelete = 'DELETE FROM client_data WHERE fullname = ?';
-
-  db.query(sqlDelete, fullname, (err, result) => {
-    if (err) {
-      console.log(err);
-    }
-  });
+  );
 });
 
 // update the amount of a client
-app.put('/api/update', (req, res) => { 
+app.put('/api/update', (req, res) => {
   const fullname = req.body.fullname;
   const amount = req.body.amount;
-  const sqlUpdate =
-    'UPDATE client_data SET amount = ? WHERE fullname = ?';
+  const id = req.body.ID;
+  const streetAddress = req.body.street_address;
+  const city = req.body.city;
+  const state = req.body.state;
+  const zip = req.body.zip;
+  const email = req.body.email;
+  const paid = req.body.paid;
+  const dueDate = req.body.dueDate;
+  const pastDue = req.body.pastDue;
+  const sqlUpdate = `UPDATE client_data SET(amount, fullname, street_address, city, state, zip, email, paid, due_date, past_due) VALUES ('${fullname}', '${amount}', '${id}', '${streetAddress}', '${city}', '${state}', '${zip}', '${email}', '${paid}', '${dueDate}', '${pastDue}')`;
 
-  db.query(sqlUpdate, [amount, fullname], (err, result) => { 
+  db.query(
+    sqlUpdate,
+    [
+      amount,
+      fullname,
+      id,
+      streetAddress,
+      city,
+      state,
+      zip,
+      email,
+      paid,
+      dueDate,
+      pastDue,
+    ],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      }
+    }
+  );
+});
+
+app.delete('/api/delete/:id', (req, res) => {
+  const id = req.params.ID;
+  const sqlDelete = `DELETE FROM client_data WHERE ID = ${id}`;
+
+  db.query(sqlDelete, id, (err, result) => {
     if (err) {
       console.log(err);
     }
